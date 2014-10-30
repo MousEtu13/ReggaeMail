8671c4b139a904560438d24fddac722fe1b829d6
mous <fekari.mustapha@gmail.com>
2014-10-30 22:34:17 +0100

    correction du code teste WhenConditionTest.java

--- src/condition/WhenConditionTest.java
+++ src/condition/WhenConditionTest.java
@@ -16,17 +16,35 @@ public class WhenConditionTest {
 	@Test
 	public void testEval() {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
-    	
+    			
+		
+		/**1er cas **/
 		try {
-			Condition c= new WhenCondition("24/08/1978");
-			assertFalse(c.eval());
-			c= new WhenCondition(sdf.format(new Date()));
-			assertTrue(c.eval());
+			Condition c= new WhenCondition("24/08/1978", null);
+			assertTrue(c.eval()==0);
+			c= new WhenCondition(sdf.format(new Date()),null);
+			assertTrue(c.eval()==1);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			assertTrue(false);
 		}
+		
+		
+		/**2 eme cas**/
+		try {
+			Condition c= new WhenCondition("24/08/1978", "23");
+			assertTrue(c.eval()==0);
+			c= new WhenCondition(sdf.format(new Date()),"23");
+			assertTrue(c.eval()==2);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			assertTrue(false);
+		}
+
+		
+
 	}
 
 }
